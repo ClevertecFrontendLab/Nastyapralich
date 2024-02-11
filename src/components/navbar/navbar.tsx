1243a35b690736c01dd39ac4e980b2df0634fc7f
Anastasia <nastyapralitch@yandex.by>
2024-02-12 00:24:25 +0300

    "fix bugs and added some adaptive"

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,81 +1,113 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive'
 import style from './navbar.module.scss';
 import {
-    CalendarFilled,
-    HeartOutlined,
-    LogoutOutlined,
-    ProfileFilled,
-    TrophyOutlined,
+  CalendarFilled,
+  HeartOutlined,
+  LogoutOutlined,
+  ProfileFilled,
+  TrophyOutlined,
 } from '@ant-design/icons';
 
 export const NavBar: React.FC = () => {
-    const [isOpen, setIsOpened] = useState(true);
-
-    const onHadleOpen = () => {
-        setIsOpened(!isOpen);
+  const [isOpen, setIsOpen] = useState(true);
+  const [dataTestId, setDataTestId] = useState('');
+  const isMobile = useMediaQuery({ query: '(max-width: 362px)' })
+  useEffect(() => {
+    const setTestData = () => {
+      let isMobile = window.outerWidth < 370;
+      setDataTestId(isMobile ? 'sider-switch-mobile' : 'sider-switch');
     };
-    return (
-            <div className={isOpen ? style.container : style.closeContainer}>
-                <div className={style.logo}>
-                    {isOpen ? (
-                        <img src='src\components\assets\logo.png' alt='logo' />
-                    ) : (
-                        <img src='src\components\assets\smallLogo.png' alt='logo' />
-                    )}
 
-                    <div>
-                        {isOpen ? (
-                            <ul>
-                                <li>
-                                    <HeartOutlined />
-                                    <p>Тренировки</p>
-                                </li>
-                                <li>
-                                    <CalendarFilled />
-                                    <p>Календарь</p>
-                                </li>
-                                <li>
-                                    <TrophyOutlined />
-                                    <p>Достижения</p>
-                                </li>
-                                <li>
-                                    <ProfileFilled />
-                                    <p>Профиль</p>
-                                </li>
-                            </ul>
-                        ) : (
-                            <ul>
-                                <li>
-                                    <HeartOutlined />
-                                </li>
-                                <li>
-                                    <CalendarFilled />
-                                </li>
-                                <li>
-                                    <TrophyOutlined />
-                                </li>
-                                <li>
-                                    <ProfileFilled />
-                                </li>
-                            </ul>
-                        )}
-                    </div>
-                </div>
-                <div>
-                    {isOpen ? (
-                        <div className={style.iconsWrap}>
-                            <LogoutOutlined />
-                            <p>Выход</p>
-                        </div>
-                    ) : (
-                        <div>
-                            <LogoutOutlined />
-                        </div>
-                    )}
-                </div>
-                <div data-test-id='sider-switch' className={style.sidemenu} onClick={onHadleOpen}>
-                    <img src='src\components\assets\sidemenu-fixed-switcher.png' alt='sidemenu' />
-                </div>
-            </div>
-    );
+    setTestData();
+
+    window.addEventListener('resize', setTestData);
+
+    return () => {
+      window.removeEventListener('resize',  setTestData);
+    };
+  }, []);
+
+  const onHandleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  
+  return (
+    <div className={isOpen ? style.container : style.closeContainer}>
+      <div className={style.logo}>
+        {isOpen && !isMobile ?  (
+          <img src='src/components/assets/logo.png' alt='logo' />
+        ) : (
+          <img src='src/components/assets/smallLogo.png' alt='logo' />
+        )}
+        <div>
+          {isOpen ? (
+            <ul>
+             {isMobile ?
+             ( <p>Тренировки</p>) :
+              (<li>
+                <HeartOutlined />
+                <p>Тренировки</p>
+              </li>)
+              }
+              {isMobile?
+               (<p>Календарь</p>) :
+              ( <li>
+                <CalendarFilled />
+                <p>Календарь</p>
+              </li>)
+             }
+              {isMobile?
+               (<p>Достижения</p>) :
+              <li>
+                <TrophyOutlined /> 
+                <p>Достижения</p>
+              </li>}
+              {isMobile? ( <p>Профиль</p>)
+              :
+              (<li>
+                <ProfileFilled />
+                <p>Профиль</p>
+              </li>)}
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                <HeartOutlined />
+              </li>
+              <li>
+                <CalendarFilled />
+              </li>
+              <li>
+                <TrophyOutlined />
+              </li>
+              <li>
+                <ProfileFilled />
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+      <div>
+        {isOpen ? (
+          <div className={style.iconsWrap}>
+            <LogoutOutlined />
+            <p>Выход</p>
+          </div>
+        ) : (
+          <div>
+            <LogoutOutlined />
+          </div>
+        )}
+      </div>
+      <div
+        data-test-id={isMobile ? 'sider-switch-mobile' : "sider-switch"}
+        className={style.sidemenu}
+        onClick={onHandleOpen}
+      >
+        <img src='src/components/assets/sidemenu-fixed-switcher.png' alt='sidemenu' />
+      </div>
+    </div>
+  );
 };
