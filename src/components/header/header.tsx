9fd058a3906b6257f1e0dbd2d4b6dad41380ec22
Anastasia <nastyapralitch@yandex.by>
2024-02-12 10:59:05 +0300

    "fixed adaptive on 864px"

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import style from './header.module.scss'
+import { useMediaQuery } from 'react-responsive'
+import { SettingFilled } from '@ant-design/icons'
 
 type HeaderProps = {
     text: string
 }
 
 export const Header: React.FC<HeaderProps> = ({text}) => {
+    const isMobile = useMediaQuery({ query: '(max-width: 362px)' })
     return <>
     <div className={style.headerContainer}>
         <div className={style.headerContainerMain}>
          <span>Главная</span>
         <div className={style.headerText}>
          <p>{text}</p>
-         <span>Настройки</span>
+         {isMobile? <span> <SettingFilled /> </span>:<span>Настройки</span>}
         </div>
         </div>
         
